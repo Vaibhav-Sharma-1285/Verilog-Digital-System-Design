e6c0af290b90d58c82f819352b328d243563f1bd
Vaibhav Sharma <vaibhavbhardwaj6412@ce.du.ac.in>
2025-11-14 18:22:21 +0530

    Updated sr_latch_flip_flop.v

--- SR Latch Flip Flop/sr_latch_flip_flop.v
+++ SR Latch Flip Flop/sr_latch_flip_flop.v
@@ -6,17 +6,7 @@ output reg q,
 output qbar
     );
     
-    always @(*) begin
-    if(s==1&r==0)
-    q=1;
-    else if(s==0&r==1)
-    q=0;
-    else if(s==0&r==0)
-    q=q;
-    else
-    q=1'bx;
-    end
-    
-    assign qbar=~q;
-    
+    assign q = ~(r | qbar);
+    assign qbar = ~(s | q);
+
 endmodule
